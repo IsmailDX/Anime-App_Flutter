7972b576bd86d8b286d1056df62e064ed46814cc
IsmailDX <ismaildxx@gmail.com>
2023-07-30 17:09:24 +0300

    Can download Images now

--- lib/screens/character_images.dart
+++ lib/screens/character_images.dart
@@ -1,11 +1,40 @@
 import 'package:anime_app/models/character.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CharacterImagesScreen extends StatelessWidget {
-  const CharacterImagesScreen({required this.character, super.key});
+  const CharacterImagesScreen({required this.character, Key? key})
+      : super(key: key);
 
   final Character character;
+
+  Future<void> _saveImageToGallery(
+      BuildContext context, String imagePath) async {
+    if (!(await Permission.storage.isGranted)) {
+      var status = await Permission.storage.request();
+      if (!status.isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Permission denied to access storage')),
+        );
+        return;
+      }
+    }
+
+    try {
+      ByteData data = await rootBundle.load(imagePath);
+      await ImageGallerySaver.saveImage(data.buffer.asUint8List());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Image saved to gallery')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to save image')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +48,47 @@ class CharacterImagesScreen extends StatelessWidget {
               itemCount: 3,
               scrollDirection: Axis.horizontal,
               itemBuilder: (ctx, index) {
-                return Container(
-                  height: double.infinity,
-                  width: 200,
-                  margin: const EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                          'assets/images/list_images/${character.name}/${character.list![index]}'),
+                String imagePath =
+                    'assets/images/list_images/${character.name}/${character.list![index]}';
+
+                return GestureDetector(
+                  onLongPress: () {
+                    showDialog(
+                      context: context,
+                      builder: (ctx) {
+                        return AlertDialog(
+                          title: const Text('Download Image'),
+                          content:
+                              const Text('Do you want to download this image?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                _saveImageToGallery(context, imagePath);
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Download'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Container(
+                    height: double.infinity,
+                    width: 200,
+                    margin: const EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(imagePath),
+                      ),
                     ),
                   ),
                 );
